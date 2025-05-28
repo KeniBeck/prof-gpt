7505f898b62379b8149e320bd053ccc3dec27be1
KeniBeck <deyler_mena69222@elpoli.edu.co>
2025-05-28 10:10:48 -0500

    feat: Refactor LoginFrom component and update Login view layout for improved styling and structure

--- src/components/ui/LoginFrom.tsx
+++ src/components/ui/LoginFrom.tsx
@@ -6,65 +6,68 @@ import { MdMarkEmailUnread } from "react-icons/md";
 import { TbPasswordFingerprint } from "react-icons/tb";
 
 const formSchema = z.object({
-    email: z.string().min(2, {
-        message: "Email is correct.",
-    }),
-    password: z.string().min(8, {
-        message: "Password must be at least 8 characters.",
-    }),
+  email: z.string().min(2, {
+    message: "Email is correct.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
 });
 const LoginFrom = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    async function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log("Form submitted with values:", values);
-    }
-    return (
-        <>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="w-[500px] bg-amber-100/20 backdrop-blur-sm border border-gray-200 shadow-lg p-4 rounded-2xl">
-                <div className="text-2xl font-bold text-gray-900">Iniciar Sesión</div>
-                <div className="text-gray-700 mb-6">Accede a tu asistente educativo personalizado</div>
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log("Form submitted with values:", values);
+  }
+  return (
+    <>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="bg-amber-100/20 backdrop-blur-sm border border-gray-200 shadow-lg px-6 py-6 max-w-lg w-[95%] rounded-2xl mx-2"
+      >
+        <div className="">
+          <div className="text-2xl font-bold text-gray-900">Iniciar Sesión</div>
+          <div className="text-gray-700 mb-6">
+            Accede a tu asistente educativo personalizado
+          </div>
 
-                <div className="space-y-6">
-                    <FloatingInput
-                        id="email"
-                        label="Correo Electrónico"
-                        type="email"
-                        registration={form.register("email")}
-                        error={form.formState.errors.password}
-                        required
-                        className="mt-4"
-                        icon={<MdMarkEmailUnread className="h-5 w-5 text-gray-500" />} />
-                    <FloatingInput
-                        id="password"
-                        label="Contraseña"
-                        type="password"
-                        registration={form.register("password")}
-                        error={form.formState.errors.password}
-                        required
-                        className="mt-4"
-                        icon={<TbPasswordFingerprint className="h-5 w-5 text-gray-500" />}
-                    />
-                    <button
-                        type="submit"
-                        className="w-full bg-red-600 hover:bg-red-700 text-white font-medium py-2 px-4 rounded-lg mb-2"
-                    >
-                        Enviar
-                    </button>
-                </div>
-
-
-
-            </form>
-        </>
-    );
-}
+          <div className="space-y-6">
+            <FloatingInput
+              id="email"
+              label="Correo Electrónico"
+              type="email"
+              registration={form.register("email")}
+              error={form.formState.errors.password}
+              required
+              className="mt-4"
+              icon={<MdMarkEmailUnread className="h-5 w-5 text-gray-500" />}
+            />
+            <FloatingInput
+              id="password"
+              label="Contraseña"
+              type="password"
+              registration={form.register("password")}
+              error={form.formState.errors.password}
+              required
+              className="mt-4"
+              icon={<TbPasswordFingerprint className="h-5 w-5 text-gray-500" />}
+            />
+            <button
+              type="submit"
+              className="w-full bg-red-600 hover:bg-red-700 text-white font-medium py-2 px-4 rounded-lg mb-2"
+            >
+              Enviar
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
 export default LoginFrom;
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -2,15 +2,21 @@ import LoginFrom from "../components/ui/LoginFrom";
 
 import MentoraSVG from "../assets/mentora.svg";
 const Login = () => {
-  return <><div className="min-h-screen bg-gradient-to-br from-amber-50 to-red-50/30 flex flex-col items-center">
-      <div className="text-center mb-2">
+  return (
+    <>
+      <div className="h-screen w-full bg-gradient-to-br from-amber-50 to-red-50/30 flex flex-col items-center justify-center">
+        <div className="text-center mb-2">
           <div className="flex justify-center">
             <img src={MentoraSVG} className="h-[150px] w-[150px]" />
           </div>
           <h1 className="text-3xl font-bold text-gray-900">Mentora AI</h1>
-          <p className="text-gray-600 mt-4">Asistente inteligente para profesores</p>
+          <p className="text-gray-600 mt-4">
+            Asistente inteligente para profesores
+          </p>
         </div>
-    <LoginFrom />
-  </div></>;
+        <LoginFrom />
+      </div>
+    </>
+  );
 };
 export default Login;
