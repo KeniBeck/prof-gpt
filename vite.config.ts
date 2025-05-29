6be7db51a15e98f23796452aee6d4b7655332088
kenibeck <deyler_mena69222@elpoli.edu.co>
2025-05-29 00:34:28 -0500

    feat: add Vite configuration for build output and asset directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Añade esta línea si tu app está en un subdirectorio
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
